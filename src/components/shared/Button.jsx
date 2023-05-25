f69ed62a0aa1243efea532e27104e5a05f691d47
Rahil Shah <rhlshah2001@gmail.com>
2023-05-24 22:13:22 -0700

    Initial commit

---
+++ src/components/shared/Button.jsx
@@ -0,0 +1,11 @@
+import React, { Children } from "react";
+
+function Button({ children, version, type, isDisabled }) {
+  return (
+    <button type={type} disabled={isDisabled} className={`btn btn-${version}`}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
